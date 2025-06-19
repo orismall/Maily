aacbac50c6c9fe55c253b8ba9807d8110ea077d4
orismall <ori.small@live.biu.ac.il>
2025-06-19 18:56:28 +0300

    fixes

--- src/Web/app.js
+++ src/Web/app.js
@@ -12,7 +12,6 @@ const spamRouter = require('./routes/spam');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
