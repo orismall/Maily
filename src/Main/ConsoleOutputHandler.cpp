0619f1181a868d08cff9bddc3a427e745d7ac542
itayturiel <itayturiel@gmail.com>
2025-06-23 19:03:22 +0300

    screens ready

--- src/Main/ConsoleOutputHandler.cpp
+++
@@ -1,7 +0,0 @@
-#include "ConsoleOutputHandler.h"
-using namespace std;
-
-// Prints the provided message to the standard console output
-void ConsoleOutputHandler::print(const string& message) {
-    cout << message << endl;
-}
--- src/Web/routes/tokens.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tokensController = require('../controllers/tokens');
-
-// Login user
-router.post('/tokens', tokensController.login);
-
-module.exports = router;
